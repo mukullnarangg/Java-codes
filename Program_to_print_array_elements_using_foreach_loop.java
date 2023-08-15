92aaa22ff6af05f622d11d0b8384f5dc655cddde
mukullnarangg <141302662+mukullnarangg@users.noreply.github.com>
2023-08-15 23:27:44 +0530

    Create Program_to_print_array_elements_using_foreach_loop.java

---
+++ Program_to_print_array_elements_using_foreach_loop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class foreach{
+	public static void main(String args[]){
+	int numbers[] = {1,2,3,2};
+	for(int number : numbers){
+		System.out.print(number+" ");
+	}
+	}
+}
