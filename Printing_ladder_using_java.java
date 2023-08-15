f41d5448b6d18311b0f87a000ab86e2834cb4aa8
mukullnarangg <141302662+mukullnarangg@users.noreply.github.com>
2023-08-15 23:17:46 +0530

    Create Printing_ladder_using_java.java

---
+++ Printing_ladder_using_java.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ladder{
+	public static void main(String[] args){
+	System.out.print("Enter number till where you want to print the ladder:");
+	Scanner num = new Scanner(System.in);
+	int i = num.nextInt();
+	for(int c=0;c<=i;c++){
+	System.out.println(c);
+	}
+	}
+	}
