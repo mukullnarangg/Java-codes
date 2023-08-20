b9a002ed7e841f8b0c382f58255df6f110185dd1
mukullnarangg <141302662+mukullnarangg@users.noreply.github.com>
2023-08-20 16:53:58 +0530

    Create Program_for_Array_Bubble_Sort.java

---
+++ Program_for_Array_Bubble_Sort.java
@@ -0,0 +1,19 @@
+public class ArrayBubbleSort {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 5, 6};
+        int n = numbers.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
