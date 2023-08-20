7f7487a2c6c05568c9a6bd6592b8b34a3e7fe785
mukullnarangg <141302662+mukullnarangg@users.noreply.github.com>
2023-08-20 15:47:15 +0530

    Create Program_for_Abstract_Parent_Static_Method.java

---
+++ Program_for_Abstract_Parent_Static_Method.java
@@ -0,0 +1,10 @@
+abstract class parent{
+	static void demofun(){
+		System.out.println("fun method is called");
+	}
+}
+public class child extends parent{
+	public static void main(String args[]){
+		parent.demofun();
+	}
+}
