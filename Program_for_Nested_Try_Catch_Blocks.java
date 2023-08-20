67eb083b16c68c475f2bf80d42f75cea0d8268bf
mukullnarangg <141302662+mukullnarangg@users.noreply.github.com>
2023-08-20 16:40:39 +0530

    Create Program_for_Nested_Try_Catch_Blocks.java

---
+++ Program_for_Nested_Try_Catch_Blocks.java
@@ -0,0 +1,21 @@
+class NestedTry{
+	public static void main(String[] args){
+	try{
+	int a = args.length;
+	int b = 43/a;
+	try{
+	if(a==1){
+	a=a/(a-a);
+	}
+	if(a==2){
+	int c[] = {1};
+	c[32] = 25;
+	}
+	}catch(ArrayIndexOutOfBoundsException e){
+	System.out.println("AIOOB" +e);
+	}
+	}catch(ArithmeticException e){
+	System.out.println("ArithmeticExc");
+	}
+	}
+}
