36ec736ca348b9418f01f5bb46d5ac666a0aaed6
mukullnarangg <141302662+mukullnarangg@users.noreply.github.com>
2023-08-20 16:53:14 +0530

    Create Program_for_Array_Selection_Sort.java

---
+++ Program_for_Array_Selection_Sort.java
@@ -0,0 +1,5 @@
+public class ArraySelectionSort {
+    public static void main(String[] args) {
+        int[] numbers = {8, 3, 1, 7, 5, 2};
+        int n = numbers.length;
+        for (int i = 0; i < n - 1; i++)
